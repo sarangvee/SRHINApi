1a9c6e0cf53aab1ca5131b7fb9038fd1f87cea73
Saravanan Velayutham <saravanan.velayutham@srholdings.in>
2025-04-28 01:15:15 +0530

    Add project files.

---
+++ SRHINApi/Features/API01.feature
@@ -0,0 +1,9 @@
+﻿Feature: API Testing for SRHIN
+
+A short summary of the feature
+
+@tag1
+Scenario: API Testing Scenario 1
+	Given load the URL "https://schoolmgmtapi-cinddev-hha5fjhxawd2hpdw.centralindia-01.azurewebsites.net/api/v1/Admin/Subscription"
+	Then verify the response status code is 200
+	
---
+++ SRHINApi/StepDefinitions/API01Step.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRHINApi.StepDefinitions
+{
+    [Binding]
+    public class API01Step
+    {
+        HttpClient httpClient;
+        HttpResponseMessage response;
+        String responseBody;
+
+        public API01Step()
+        {
+            httpClient = new HttpClient();
+        }
+
+        [Given("load the URL {string}")]
+        public async Task GivenLoadTheURL(string uri)
+        {
+            response= await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            responseBody= await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Test URL {0}", uri);
+            Console.WriteLine("Test Body {0}", responseBody);
+        }
+
+        [Then("verify the response status code is {int}")]
+        public void ThenVerifyTheResponseStatusCodeIs(int statuscode)
+        {
+            Console.WriteLine("Then verify the response status code is {0}", statuscode);
+        }
+
+    }
+}
